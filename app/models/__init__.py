8b1d23a5d168ac75b4ee23e0586cc1128408cfe7
Birobizhan <fedorovd2005@gmail.ru>
2025-11-26 16:51:29 +0300

    Добавил alembic миграции

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.list import List
+from app.models.movie import Movie
+from app.models.review import Review
+from app.models.user import User
+
+__all__ = ['List', 'Movie', 'Review', 'User']
