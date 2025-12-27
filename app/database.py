6737feb886a65bc812557556854bad1f040bf5cb
Birobizhan <fedorovd2005@gmail.com>
2025-12-27 10:15:54 +0300

    Небольшие изменения

--- app/database.py
+++ app/database.py
@@ -1,2 +1,2 @@
-from app.db.base import Base
-from app.db.session import SessionLocal, engine, get_db
+from app.db.base import Base  # noqa: F401
+from app.db.session import SessionLocal, engine, get_db  # noqa: F401
